bd320fd603001c4f4501a57d1ee87b09feb8928f
Xavier Perez <perezxavier918@gmail.com>
2021-10-21 10:03:01 -0600

    Agregar archivos de proyecto.

---
+++ Practica 4 Ej 1/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_4_Ej_1
+{
+    class Persona
+    {
+        private string nombre;
+        private int edad;
+        private string sexo;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        public string Sexo
+        {
+            get { return sexo; }
+            set { sexo = value; }
+        }
+    }
+}
