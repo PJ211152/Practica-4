14b80ed7130beee55ae93cabf6bba6a28e12b7c2
Xavier Perez <perezxavier918@gmail.com>
2021-10-21 10:04:50 -0600

    Cambios 1.1

--- Practica 4 Ej 1/Form1.Designer.cs
+++ Practica 4 Ej 1/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(695, 365);
+            this.ClientSize = new System.Drawing.Size(691, 358);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
